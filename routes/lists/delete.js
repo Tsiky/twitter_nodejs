81eda4109375c82d2681549e4efa32c30d094a3b
Mattys <herve.mattys@gmail.com>
2017-12-14 03:21:16 -0800

    CHange in name convention for delete list routes

--- routes/lists/delete.js
+++ routes/lists/delete.js
@@ -4,18 +4,19 @@ var async = require("async");
 var t = require('../../twitter/twitter_connection');
 
 router.delete('/', function (req, res, next) {
-    
     var calls = [];
     //Get all the tweets from the authenticated user, not including his retweets
-    t.get('direct_messages', function (err, data, response) {
+    t.get('lists/ownerships', function (err, data, response) {
         if (err) {
             res.status(err.statusCode).send(err.message);
         } else {
-            var strQuery = req.query.query;
+            var strQuery = req.query.name;
             //Push all the id of tweets having the query string in their text in queue for deletion
             data.forEach(function (element) {
-                if (element.text.indexOf(strQuery) !== -1)
-                    calls.push(deleteDirectMessages.bind(null, element.id_str));
+                getMembers(element.id).forEach(function (el) {
+                    if (el.name.indexOf(strQuery) !== -1)
+                        calls.push(deleteList.bind(null, element.id_str));
+                });
             });
             //Delete all the matching tweets in parallel
             async.parallel(
@@ -31,9 +32,20 @@ router.delete('/', function (req, res, next) {
     });
 });
 
+function getMembers(id, callback) {
+    t.get('lists/member', {list_id: id}, function (err, data, response) {
+        if (err) {
+            console.log(err);
+            callback(err, null);
+        } else {
 
-function deleteDirectMessages(id, callback) {
-    t.post('direct_messages/destroy', {id: id}, function (err, data, response) {
+            callback(null, data);
+            return data;
+        }
+    });
+}
+function deleteList(id, callback) {
+    t.post('lists/destroy', {list_id: id}, function (err, data, response) {
         if (err) {
             console.log(err);
             callback(err, null);
--- routes/lists/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.delete('/', function(req, res, next) {
-	res.send('DELETE list containing '+req.query.query_str);
-});
-
-module.exports = router;
