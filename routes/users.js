402388af6afedf8def8690018819a89c47c30676
Tsiky <tsiky.andriam@gmail.com>
2017-12-14 13:47:47 +0100

    FEATURE: Add package for hateoas

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    var user = {
+        name: 'Trump',
+        id: '25073877'
+    };
+    res.status(205).json(user, [
+        { rel: "self", method: "GET", href: 'http://127.0.0.1' },
+        { rel: "follow", method: "POST", title: 'Follow user', href: 'https://api.twitter.com/1.1/friendships/create?user_id=' + user.id }
+    ]);
+    // res.send('respond with a resource');
 });
 
 module.exports = router;
