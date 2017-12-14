c827e187d12974d06a7aa2514918fce31d206ed9
Tsiky <tsiky.andriam@gmail.com>
2017-12-14 18:44:00 +0100

    MODIF: change empty tokens

--- twitter/twitter_connection.js
+++ twitter/twitter_connection.js
@@ -17,11 +17,13 @@ var setCredentials = function (headers) {
             access_token_secret: headers.access_token_secret
         })
     }
+
+    // Return error
     return new Twit({
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token: process.env.ACCESS_TOKEN,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+        consumer_key: 'consumer_key',
+        consumer_secret: 'consumer_secret',
+        access_token: 'access_token',
+        access_token_secret: 'access_token_secret'
     })
 };
 
