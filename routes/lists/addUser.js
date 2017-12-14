3e1f7403cef9a42a8ddfcf41ce9db5552666b401
slvcsl <silvia.casola8@gmail.com>
2017-12-14 09:58:03 +0100

    addUser added

--- routes/lists/addUser.js
+++ routes/lists/addUser.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
 var t = require('../../twitter/twitter_connection');
 
 router.put('/', function (req, res, next) {
-    res.send('Add a user to all lists of the authenticated user');
+    t.get('lists/ownerships', function (err, data, response) {
+        if (err) {
+            res.status(err.statusCode).send(err.message);
+        }
+        else {
+            var all_lists = data.lists;
+            var calls = []; // Array of calls to function addUser
+            for (var i = 0; i < all_lists.length; i++) {
+                calls.push(addUser.bind(null, all_lists[i], req.query.id))
+            }
+
+            // Add to all the blocks in parallel and wait for the response of all the calls
+            async.parallel(
+                calls,
+                function (err, results) {
+                    if (err) {
+                        res.status(err.statusCode).send(err.message);
+                    }
+                    else {
+                        res.send(202);
+                    }
+                });
+        }
+    });
 });
+
+function addUser(list, userid, callback) {
+    list_slug = list.slug;
+    list_owner = list.user.id;
+    t.post('lists/members/create', { user_id: userid, slug: list_slug, owner_id: list_owner }, function (err, data, response) {
+        if (err) {
+            console.log(err);
+            callback(err, null);
+        }
+        else {
+            console.log(data);
+            callback(null, data);
+        }
+    });
+}
 
 module.exports = router;
